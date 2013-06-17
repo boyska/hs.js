dde67fe580db06a0f2114d33e1018a69552f4667
boyska <piuttosto@logorroici.org>
2013-06-17 19:50:56 +0200

    cleanup hs.py and hsadmin.py

--- hs.py
+++ hs.py
@@ -1,62 +1,59 @@
-'''
+"""
 HS utils
-'''
+"""
 
 import sys
 import os
-sys.path.append('libs/torctl/TorCtl')
-from TorCtl import Connection, DebugEventHandler
 import socket
+from contextlib import contextmanager
+
+sys.path.append('libs/torctl/TorCtl')
+from TorCtl import Connection, EventHandler
 
 class HiddenService(object):
-    '''Hidden service information are kept here. Not much more'''
+    """Hidden service information are kept here. Not much more"""
     def __init__(self, hsdir, onionport, address):
         self.hsdir = hsdir
         self.onionport = onionport
         self.address = address
         self.onion = get_onion_host_by_dir(hsdir)
 
-    @classmethod
-    def get_by_onion_port(self, onion, port):
-        raise NotImplementedError()
-
-    @classmethod
-    def get_by_address(self, host, port):
-        raise NotImplementedError()
-
-    @classmethod
-    def get_by_dir(self, hsdir):
-        raise NotImplementedError()
-
 def get_onion_host_by_dir(hsdir):
+    """Given a path to a directory, assumes it is the directory of a hidden
+    service, and return sth like abcdefghij123456.onion"""
     return open(os.path.join(hsdir, 'hostname')).read().strip()
 
+@contextmanager
 def get_tor_control(host='127.0.0.1', port=9051):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,port))
-    c = Connection(s)
-    c.set_event_handler(DebugEventHandler())
-    th = c.launch_thread()
-    c.authenticate()
-    return c
+    """Context manager: yields a connection to the tor control port"""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    conn = Connection(sock)
+    conn.set_event_handler(EventHandler())
+    conn.launch_thread()
+    conn.authenticate()
+    yield conn
+    conn.close()
+    sock.close()
 
 def get_hidden_services():
-    c = get_tor_control()
-    opt = c.get_option('HiddenServiceOptions')
-    if len(opt) == 1:
-        return
-    for i in xrange(0, len(opt)-1, 2):
-        opt_dict = dict((k,v) for (k,v) in opt[i:i+2])
-        print 'opt group', opt_dict
-        yield HiddenService(opt_dict['HiddenServiceDir'],
-                *opt_dict['HiddenServicePort'].split())
+    """A generator of HiddenService instances"""
+    with get_tor_control() as control:
+        opt = control.get_option('HiddenServiceOptions')
+        if len(opt) == 1:
+            return
+        for i in xrange(0, len(opt)-1, 2):
+            opt_dict = dict((k, v) for (k, v) in opt[i:i+2])
+            print 'opt group', opt_dict
+            yield HiddenService(opt_dict['HiddenServiceDir'],
+                    *opt_dict['HiddenServicePort'].split())
 
 def add_hidden_service(hsdir, onionport, port, host='127.0.0.1'):
-    c = get_tor_control()
-    curhs = c.get_option('HiddenServiceOptions')
-    if len(curhs) == 1:
-        curhs = []
-    new = curhs + [("HiddenServiceDir", hsdir),
-          ("HiddenServicePort", "%d %s:%d" % (onionport,host,port))]
-    c.set_options(new)
-
+    """Add a new hidden service. Does not check if there are conflicts"""
+    with get_tor_control() as control:
+        curhs = control.get_option('HiddenServiceOptions')
+        if len(curhs) == 1:
+            curhs = []
+        new = curhs + [("HiddenServiceDir", hsdir),
+              ("HiddenServicePort", "%d %s:%d" % (onionport,host,port))]
+        control.set_options(new)
--- hsadmin.py
+++ hsadmin.py
@@ -1,34 +1,46 @@
+"""
+Simple tool to manage Tor Hidden Services
+
+If you want to create/remove/list hidden services on your tor instance without
+messing with your torrc, this is for you.
+"""
+import argparse
+
 from hs import get_hidden_services, add_hidden_service
 
 def hs_list(args):
-    print 'List of HS:'
+    """List active hidden services as
+    $url.onion:78\t$host:port\t/da/ta/dir
+    """
     print '\n'.join((hs.onion for hs in get_hidden_services()))
 
 def hs_add(args):
-    import random
-    #onionport = random.randint(10000,20000)
+    """Add a hidden service"""
     onionport = 12345
     port = 23456
-    #port = random.randint(20000,30000)
     add_hidden_service(hsdir='/var/lib/tor/testhey_hs',
             onionport=onionport, port=port)
 def hs_rm(args):
+    """Remove a hidden service"""
     raise NotImplementedError()
 
-import sys
-import argparse
+def parse_and_run(system_arguments):
+    '''Main function'''
+    parser = argparse.ArgumentParser(prog='hs')
+    subparsers = parser.add_subparsers(title='subcommands',
+            description='valid subcommands', help='additional help')
 
-parser = argparse.ArgumentParser(prog='hs')
-subparsers = parser.add_subparsers(title='subcommands',
-        description='valid subcommands', help='additional help')
+    parser_ls = subparsers.add_parser('list',
+            help='list active hidden services')
+    parser_ls.set_defaults(func=hs_list)
+    parser_add = subparsers.add_parser('add', help='add a hidden service')
+    parser_add.set_defaults(func=hs_add)
+    parser_rm = subparsers.add_parser('rm', help='remove a hidden service')
+    parser_rm.set_defaults(func=hs_rm)
 
-parser_ls = subparsers.add_parser('list', help='list active hidden services')
-parser_ls.set_defaults(func=hs_list)
-parser_add = subparsers.add_parser('add', help='add a hidden service')
-parser_add.set_defaults(func=hs_add)
-parser_rm = subparsers.add_parser('rm', help='remove a hidden service')
-parser_rm.set_defaults(func=hs_rm)
+    args = parser.parse_args(system_arguments)
+    args.func(args)
 
-args = parser.parse_args(sys.argv[1:])
-args.func(args)
-
+if __name__ == '__main__':
+    import sys
+    parse_and_run(sys.argv[1:])
