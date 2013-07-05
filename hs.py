8bf3571bf43c320c0c9fc8b02466cf07f07b6254
dvd <dvd@denn.is>
2013-07-05 18:15:28 +0200

    --format, --debug, --localport, logging, fixes

--- hs.py
+++ hs.py
@@ -2,13 +2,15 @@
 HS utils
 """
 
-import sys
 import os
-import socket
 from contextlib import contextmanager
+import logging
 
-sys.path.append('libs/torctl/TorCtl')
+from pprint import pformat
+
 from stem.control import Controller
+
+logger = logging.getLogger('hs')
 
 class HiddenService(object):
     """Hidden service information are kept here. Not much more"""
@@ -17,6 +19,8 @@ class HiddenService(object):
         self.onionport = int(onionport)
         self.address = address
         self.onion = get_onion_host_by_dir(hsdir)
+    def __getitem__(self, key):
+        return getattr(self, key)
 
 def get_onion_host_by_dir(hsdir):
     """Given a path to a directory, assumes it is the directory of a hidden
@@ -28,13 +32,20 @@ def get_tor_control(host='127.0.0.1', port=9051):
     """Context manager: yields a connection to the tor control port"""
     with Controller.from_port(port = port) as controller:
         controller.authenticate()
+        logger.debug("got tor controller")
         yield controller
 
 
 def get_hidden_services():
     """A generator of HiddenService instances"""
     with get_tor_control() as control:
-        dirs, ports = sorted(control.get_conf_map('HiddenServiceOptions').items())
+        options = control.get_conf_map('HiddenServiceOptions').items()
+        if len(options) == 2:
+            dirs, ports = sorted(options)
+        elif len(options) < 2:
+            return
+        else:
+            raise Exception("Problems parsing options")
         for hsdir, hsport in zip(dirs[1], ports[1]):
             yield HiddenService(hsdir,
                     *hsport.split())
@@ -42,12 +53,21 @@ def get_hidden_services():
 def add_hidden_service(hsdir, onionport, port, host='127.0.0.1'):
     """Add a new hidden service. Does not check if there are conflicts"""
     with get_tor_control() as control:
-        curhs_dir, curhs_port = sorted(control.get_conf_map('HiddenServiceOptions').items())
+        options = control.get_conf_map('HiddenServiceOptions').items()
+        logger.debug("current options:\n%s" % pformat(options))
+        if len(options) == 2:
+            curhs_dir, curhs_port = (o[1] for o in sorted(options))
+        elif len(options) < 2:
+            curhs_dir = []
+            curhs_port = []
+        else:
+            raise Exception("Problems parsing options")
+
         curhs_dir += (hsdir,)
         curhs_port += ("%d %s:%d" % (onionport,host,port),)
-        new = {"HiddenServiceDir": curhs_dir,
-              "HiddenServicePort": curhs_port}
-        print new
+        new = [("HiddenServiceDir", curhs_dir),
+              ("HiddenServicePort", curhs_port)]
+        logger.debug('setting options %s' % pformat(new))
         control.set_options(new)
 
 # vim: set sw=4 sw=4 expandtab:
--- hsadmin.py
+++ hsadmin.py
@@ -6,45 +6,70 @@ messing with your torrc, this is for you.
 """
 import argparse
 import sys
+import logging
 
 from hs import get_hidden_services, add_hidden_service
+from hs import logger as hs_logger
 out_buffer = sys.stdout
 
 def hs_list(args):
     """List active hidden services as
     $url.onion:78\t$host:port\t/da/ta/dir
     """
-    for hs in get_hidden_services():
-	print '%s:%d\t%s\t%s' % \
-            (hs.onion, hs.onionport, hs.address, hs.hsdir)
+    is_ok = lambda x: True
+    if args.localport:
+        is_ok = lambda x: x.address.split(':')[1] == args.localport
+    for hs in filter(is_ok, get_hidden_services()):
+        print args.fmt % hs
 
 def hs_add(args):
     """Add a hidden service"""
     onionport = int(args.onionport)
     port = int(args.localport)
-    add_hidden_service(hsdir='/var/lib/tor/testhey%d_hs' % port,
+    hsdir = args.hsdir if args.hsdir is not None else '/var/lib/tor/testhey%d_hs' % port
+    add_hidden_service(hsdir=hsdir,
             onionport=onionport, port=port)
 def hs_rm(args):
     """Remove a hidden service"""
     raise NotImplementedError()
 
+def setup_logger(logger):
+    #TODO: add option
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+
 def parse_and_run(system_arguments):
     '''Main function'''
+
     parser = argparse.ArgumentParser(prog='hs')
+    parser.add_argument("--debug", action="store_true", default=False)
     subparsers = parser.add_subparsers(title='subcommands',
             description='valid subcommands', help='additional help')
 
     parser_ls = subparsers.add_parser('list',
             help='list active hidden services')
     parser_ls.set_defaults(func=hs_list)
+    parser_ls.add_argument("-p", "--localport", help='filtering based on local port the HS binds to')
+    parser_ls.add_argument("-f", "--format", help='line format, default is %%(onion)s:%%(onionport)d\t%%(address)s\t%%(hsdir)s',
+		    dest="fmt", default='%(onion)s:%(onionport)d\t%(address)s\t%(hsdir)s' )
     parser_add = subparsers.add_parser('add', help='add a hidden service')
     parser_add.add_argument("onionport", help='the port that will be exposed publicly')
     parser_add.add_argument("localport", help='the port the hiddenservice will connect to')
+    parser_add.add_argument("-d", "--hsdir", help='the directory of the hidden service')
     parser_add.set_defaults(func=hs_add)
     parser_rm = subparsers.add_parser('rm', help='remove a hidden service')
     parser_rm.set_defaults(func=hs_rm)
 
     args = parser.parse_args(system_arguments)
+    if args.debug:
+        setup_logger(hs_logger)
     args.func(args)
 
 if __name__ == '__main__':
