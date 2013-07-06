89909558c7136c7282f4c53dfd3d60686ae2ce63
dvd <dvd@denn.is>
2013-07-06 15:14:09 +0200

    remove list -p, some cleanups

--- hsadmin.py
+++ hsadmin.py
@@ -12,26 +12,27 @@ from hs import get_hidden_services, add_hidden_service
 from hs import logger as hs_logger
 out_buffer = sys.stdout
 
+
 def hs_list(args):
-    """List active hidden services as
-    $url.onion:78\t$host:port\t/da/ta/dir
-    """
-    is_ok = lambda x: True
-    if args.localport:
-        is_ok = lambda x: x.address.split(':')[1] == args.localport
-    for hs in filter(is_ok, get_hidden_services()):
-        print args.fmt % hs
+    """List active hidden services according to args.fmt"""
+    for hs in get_hidden_services():
+        print args.fmt.replace(r'\t', '\t') % hs
+hs_list.default_format = r'%(onion)s:%(onionport)d\t%(address)s\t%(hsdir)s'
 
 def hs_add(args):
     """Add a hidden service"""
     onionport = int(args.onionport)
     port = int(args.localport)
-    hsdir = args.hsdir if args.hsdir is not None else '/var/lib/tor/testhey%d_hs' % port
+    hsdir = args.hsdir if args.hsdir is not None else\
+        '/var/lib/tor/testhey%d_hs' % port
     add_hidden_service(hsdir=hsdir,
-            onionport=onionport, port=port)
+                       onionport=onionport, port=port)
+
+
 def hs_rm(args):
     """Remove a hidden service"""
     raise NotImplementedError()
+
 
 def setup_logger(logger):
     #TODO: add option
@@ -45,22 +46,29 @@ def setup_logger(logger):
     # add ch to logger
     logger.addHandler(ch)
 
+
 def parse_and_run(system_arguments):
     '''Main function'''
 
     parser = argparse.ArgumentParser(prog='hs')
     parser.add_argument("--debug", action="store_true", default=False)
     subparsers = parser.add_subparsers(title='subcommands',
-            description='valid subcommands', help='additional help')
+                                       description='valid subcommands',
+                                       help='additional help')
 
     parser_ls = subparsers.add_parser('list',
-            help='list active hidden services')
+                                      help='list active hidden services')
     parser_ls.set_defaults(func=hs_list)
-    parser_ls.add_argument("-p", "--localport", help='filtering based on local port the HS binds to')
-    parser_ls.add_argument("-f", "--format", help='line format, default is %%(onion)s:%%(onionport)d\t%%(address)s\t%%(hsdir)s',
-		    dest="fmt", default='%(onion)s:%(onionport)d\t%(address)s\t%(hsdir)s' )
+    parser_ls.add_argument("-f", "--format",
+                           help='line format, default is ' +
+                           hs_list.default_format.replace('%',
+                                                          '%%'),
+                           dest="fmt",
+                           default=hs_list.default_format
+                           )
     parser_add = subparsers.add_parser('add', help='add a hidden service')
-    parser_add.add_argument("onionport", help='the port that will be exposed publicly')
+    parser_add.add_argument("onionport",
+                            help='the port that will be exposed publicly')
     parser_add.add_argument("localport", help='the port the hiddenservice will connect to')
     parser_add.add_argument("-d", "--hsdir", help='the directory of the hidden service')
     parser_add.set_defaults(func=hs_add)
